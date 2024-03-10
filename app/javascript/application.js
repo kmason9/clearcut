b6d5ce1c34dffe10b5f1dd0da6c6889f19b4bf9b
Kirsty Mason <kirsty_mason9@hotmail.com>
2024-03-10 00:06:12 +0000

    videos - started javascript to hover

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import VideoController from "./controllers/video_controller.js"
+Stimulus.register("video-play", VideoController)
